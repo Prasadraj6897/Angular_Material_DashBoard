47d59b4951dc5cee46933306fc023ab501f20c89
Prasadraj6897 <prasadraj32895@gmail.com>
2021-08-23 10:08:19 +0530

    Created dashB in module folder and connected to default module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: DefaultComponent,
-		// children: [{
-		//   path: '',
-		//   component: DashboardComponent
-		// },
+		children: [
+			{
+				path: '',
+				component: DashboardComponent
+			},
+		]
 	}
 ];
 
